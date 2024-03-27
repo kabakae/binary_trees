1dacb06ee7bda42de5b382527aa49e836e04fc8e
kabakae <kabakaevans@gmail.com>
2024-03-27 11:52:56 -0700

    000

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,7 +11,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		func(tree && func);
+		func(tree->func);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
