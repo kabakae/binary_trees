9f2a6eb26627c989a4dbddadab0eaa5b6356637c
kabakae <kabakaevans@gmail.com>
2024-03-27 05:30:17 -0700

    000

---
+++ binary_trees.h
@@ -0,0 +1,60 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
+ * enum rb_color_e - Possible color of a Red-Black tree
+ *
+ * @RED: 0 -> Red node
+ * @BLACK: 1 -> Black node
+ */
+typedef enum rb_color_e
+{
+	RED = 0,
+	BLACK
+} rb_color_t;
+
+/**
+ * struct rb_tree_s - Red-Black tree node structure
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @color: Color of the node (RED or BLACK)
+ */
+typedef struct rb_tree_s
+{
+	int n;
+	struct rb_tree_s *parent;
+	struct rb_tree_s *left;
+	struct rb_tree_s *right;
+	rb_color_t color;
+} rb_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+void rb_tree_print(const rb_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */
