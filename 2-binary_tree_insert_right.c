6ef28c0d428186b2e6a100362a68381f5e430a97
kabakae <kabakaevans@gmail.com>
2024-03-27 10:18:48 -0700

    000

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - ........
+ * @parent: ......
+ * @value: .........
+ * Return: ........
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+
+	new_node = binary_tree_node(parent, value);
+	if (parent->right != NULL)
+	{
+		new_node = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	return (new_node);
+}
